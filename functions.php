70244ddac88a6d7a7233a7d02bf8dddc13d2ee06
Jami Gibbs <jami0821@gmail.com>
2015-05-18 09:02:26 -0500

    Remove un-used post format support, closes #4

--- functions.php
+++ functions.php
@@ -60,14 +60,6 @@ function gateway_setup() {
 		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
 	) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	// add_theme_support( 'post-formats', array(
-	// 	'aside', 'image', 'video', 'quote', 'link',
-	// ) );
-
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'gateway_custom_background_args', array(
 		'default-color' => 'ffffff',
