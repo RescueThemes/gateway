b7bb7c25d407b29e3f4eaf73e0a317d81a7614c6
Jami Gibbs <jami0821@gmail.com>
2015-07-08 14:19:29 -0500

    Height of header will adjust with content, closes #8

--- customizer/styles.php
+++ customizer/styles.php
@@ -151,6 +151,7 @@ function customizer_library_gateway_styles() {
 		-moz-background-size: cover;
 		-o-background-size: cover;
 		background-size: cover;
+		height: 100%;
 	}
 
 	.bg-center-center {
