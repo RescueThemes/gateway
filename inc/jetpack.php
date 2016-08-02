afc6f2858fa185ebc8d60b7c7b19d446ce78167d
Rescue Themes <hello@rescuethemes.com>
2016-08-02 13:31:33 -0400

    Add infinite scroll for jetpack

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -8,12 +8,22 @@
 
 /**
  * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
+ * @link http://jetpack.me/support/infinite-scroll/
+ *
  */
 function gateway_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
 		'footer'    => 'page',
+		'render'    => 'gateway_infinite_scroll_render',
+		'wrapper'	=> false,
 	) );
 }
 add_action( 'after_setup_theme', 'gateway_jetpack_setup' );
+
+function gateway_infinite_scroll_render() {
+	while( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+}
